8ae14e5c923dd86a8fbdf3dbb081a75f9a5554db
Sylphrena Kleinsasser <ksylphrena@gmail.com>
2024-11-07 22:43:32 -0500

    use mixin to reduce duplicated styles for star animation

--- src/sections/Stars.tsx
+++ src/sections/Stars.tsx
@@ -5,9 +5,9 @@ import './Stars.scss';
 function Stars() {
   return (
     <div id="hero-star">
-      <div id='stars'></div>
-      <div id='stars2'></div>
-      <div id='stars3'></div>
+      <div id='small-stars' />
+      <div id='medium-stars' />
+      <div id='large-stars' />
     </div>
   );
 }
